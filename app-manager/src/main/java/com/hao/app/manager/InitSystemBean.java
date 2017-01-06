dc48b8aa257800896647c35de9196779dce4620c
燕伟 <yanwei@juanpi.com>
2017-01-06 14:07:51 +0800

    x

--- app-manager/src/main/java/com/hao/app/manager/InitSystemBean.java
+++ app-manager/src/main/java/com/hao/app/manager/InitSystemBean.java
@@ -30,7 +30,7 @@ public class InitSystemBean implements InitializingBean, ServletContextAware {
 	@Override
 	public void setServletContext(ServletContext servletContext) {
 		Set<String> allPrivileges = sysPrivilegeService.reLoadAllPrivilegeSet();
-		logger.info("加载系统所有权限={}", allPrivileges);
+		logger.info("加载系统所有权限:{}", allPrivileges);
 
 		// 设置页面用的全局常量
 		servletContext.setAttribute("IMAGEURL", CustomPropertyConfigurer.getProperty(Constants.CONFIG_KEY_UPFILE_URL));
