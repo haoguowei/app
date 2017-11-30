a58749dfcd0de0fa3b61fd9b6c971bbd704b2e59
haoguowei <haoguowei@youzan.com>
2017-11-30 21:20:11 +0800

    init

--- app-manager/src/main/java/com/hao/app/manager/controller/ProductPartsController.java
+++ app-manager/src/main/java/com/hao/app/manager/controller/ProductPartsController.java
@@ -1,5 +1,8 @@
 package com.hao.app.manager.controller;
 
+import com.hao.app.service.ProductPartsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,5 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class ProductPartsController extends BaseController{
+
+    @Autowired
+    private ProductPartsService productPartsService;
 
 }
