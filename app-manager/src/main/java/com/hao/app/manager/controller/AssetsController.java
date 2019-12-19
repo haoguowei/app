eb7e8fccc0765615f1bfbdaef963340b75215bcf
haoyoyo <haoguowei52@163.com>
2019-12-19 10:18:08 +0800

    Create AssetsController.java

---
+++ app-manager/src/main/java/com/hao/app/manager/controller/AssetsController.java
@@ -0,0 +1,48 @@
+package com.hao.app.manager.controller;
+
+import com.hao.app.commons.entity.result.JsonResult;
+import com.hao.app.commons.enums.ResultCodeEnum;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@Controller
+public class AssetsController extends BaseController {
+
+    private Logger logger = LoggerFactory.getLogger(AssetsController.class);
+
+    @Resource
+    private AssetsService assetsService;
+
+
+    @RequestMapping("/initAssets.do")
+    public String initAssets(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        return "jsp/assets/initAssets";
+    }
+
+
+    @RequestMapping("/searchAssets.do")
+    public void searchAssets(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       
+    }
+
+    @RequestMapping("/initAssetsEdit.do")
+    public String initAssetsEdit(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       
+        return "jsp/assets/initAssetsEdit";
+    }
+
+    @RequestMapping("/saveAssets.do")
+    public String saveAssets(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
+}
