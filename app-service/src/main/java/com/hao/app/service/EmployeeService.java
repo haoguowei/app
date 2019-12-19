9f55608e6a30116d76685bbf3ad4475714fcf61f
haoyoyo <haoguowei52@163.com>
2019-12-19 10:05:18 +0800

    Create EmployeeService.java

---
+++ app-service/src/main/java/com/hao/app/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.hao.app.service;
+
+import com.hao.app.commons.entity.result.JsonResult;
+import com.hao.app.commons.enums.ResultCodeEnum;
+import com.hao.app.pojo.AreaDO;
+
+public interface EmployeeService {
+
+    JsonResult<EmployeeDO> searchEmployee();
+
+    EmployeeDO getById(int id);
+
+    ResultCodeEnum insert(EmployeeDO employee);
+
+    ResultCodeEnum update(EmployeeDO employee);
+}
