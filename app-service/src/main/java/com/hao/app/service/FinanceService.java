8d3a8b1f874dc573b5b96bf29929512a2d4761ed
haoyoyo <haoguowei52@163.com>
2019-12-19 10:06:43 +0800

    Create FinanceService.java

---
+++ app-service/src/main/java/com/hao/app/service/FinanceService.java
@@ -0,0 +1,16 @@
+package com.hao.app.service;
+
+import com.hao.app.commons.entity.result.JsonResult;
+import com.hao.app.commons.enums.ResultCodeEnum;
+import com.hao.app.pojo.FinanceDO;
+
+public interface FinanceService {
+
+    JsonResult<FinanceDO> searchFinance();
+
+    FinanceDO getById(int id);
+
+    ResultCodeEnum insert(FinanceDO finance);
+
+    ResultCodeEnum update(FinanceDO finance);
+}
