ad009a2965bdaeee62d2db91c0ae232fc3d43992
haoyoyo <haoguowei52@163.com>
2019-12-19 09:53:35 +0800

    Update FinanceMapper.java

--- app-dao/src/main/java/com/hao/app/dao/FinanceMapper.java
+++ app-dao/src/main/java/com/hao/app/dao/FinanceMapper.java
@@ -3,16 +3,10 @@ package com.hao.app.dao;
 import com.hao.app.pojo.FinanceDO;
 
 public interface FinanceMapper {
-
-    int deleteByPrimaryKey(Integer id);
-
+    
     int insert(FinanceDO record);
-
-    int insertSelective(FinanceDO record);
 
     FinanceDO selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(FinanceDO record);
-
-    int updateByPrimaryKey(FinanceDO record);
+    int update(FinanceDO record);
 }
