9a5d3ae2baac7e0781b17f29ac50d9c400a94cdd
haoyoyo <haoguowei52@163.com>
2019-12-19 09:50:46 +0800

    Update AssetsMapper.java

--- app-dao/src/main/java/com/hao/app/dao/AssetsMapper.java
+++ app-dao/src/main/java/com/hao/app/dao/AssetsMapper.java
@@ -5,15 +5,9 @@ import com.hao.app.pojo.AssetsDO;
 
 public interface AssetsMapper {
 
-    int deleteByPrimaryKey(Integer id);
-
     int insert(AssetsDO record);
-
-    int insertSelective(AssetsDO record);
 
     AssetsDO selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(AssetsDO record);
-
-    int updateByPrimaryKey(AssetsDO record);
+    int update(AssetsDO record);
 }
