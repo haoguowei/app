fd6d66b3d4503f099eb0d625f251099d73f74d46
guowei <haoguowei52@163.com>
2019-12-26 08:43:56 +0800

    nothine

--- app-commons/src/main/java/com/hao/app/commons/entity/param/EmployeeQueryParam.java
+++ app-commons/src/main/java/com/hao/app/commons/entity/param/EmployeeQueryParam.java
@@ -15,6 +15,9 @@ import java.io.Serializable;
 public class EmployeeQueryParam extends QueryParam implements Serializable {
 
 	private static final long serialVersionUID = 4737617179006429526L;
+
+	private Integer projectsId;
+
 	private String name;
 	private String idCard;
 
